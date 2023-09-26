0e520d7c891accedea014e1030c3e0e5e854f6e4
Георгий Болезин <g.bolezin@zenith24.ru>
2023-09-26 07:09:36 +0700

    4-th commit of 10-th sprint

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import permissions, viewsets
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from .permissions import IsAuthorOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 from posts.models import Group, Post
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -33,12 +27,15 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
+    def get_current_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+
     def get_queryset(self):
-        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        post = self.get_current_post()
         return post.comments.all()
 
     def perform_create(self, serializer):
         serializer.save(
             author=self.request.user,
-            post=get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+            post=self.get_current_post()
         )
